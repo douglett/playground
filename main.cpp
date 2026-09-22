217c2744a31e94634a6f0342ad3bffec354a2185
Doug Inman <doug@totem9.net>
2026-09-22 22:05:33 +0100

    fullscreen tests

--- main.cpp
+++ main.cpp
@@ -7,15 +7,24 @@ GFX gfx;
 
 int main() {
 	printf("starting Playground...\n");
+	// setup window
 	gfx.screen.width = 400;
 	gfx.screen.height = 300;
 	gfx.screen.zoom = 2;
-	// gfx.qbfont.selectedfont = 1;
-	// SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	gfx.init();
-	SetWindowState(FLAG_WINDOW_RESIZABLE);
+	// SetWindowState(FLAG_WINDOW_RESIZABLE);
 
 	while (!gfx.shouldQuit()) {
+		if ((IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT)) && IsKeyPressed(KEY_ENTER)) {
+			IsWindowState(FLAG_BORDERLESS_WINDOWED_MODE)
+				? ClearWindowState(FLAG_BORDERLESS_WINDOWED_MODE)
+				: SetWindowState(FLAG_BORDERLESS_WINDOWED_MODE);
+		}
+
+		auto z = gfx.screen.zoom;
+		DrawRectangle(10, 10, (GetScreenWidth()/z)-20, (GetScreenHeight()/z)-20, Color{64, 0, 255, 128});
+
 		DrawTexture(gfx.qbfont.qb13.texture, 10, 100, WHITE);
 		gfx.print("Hello World!", 10, 10, BLUE);
 		gfx.flip();
