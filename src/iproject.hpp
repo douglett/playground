f9bbac0541ff2093847c566f986bcc594a2a2abf
Doug Inman <doug@totem9.net>
2026-09-23 15:46:53 +0100

    improved source file handling

--- src/iproject.hpp
+++ src/iproject.hpp
@@ -1,53 +1,74 @@
 #pragma once
 #include <fstream>
 
-// interpreter project
-struct IProject {
-	struct File { string path, name; string fname() { return path+name; } };
-	File ipfile = { "./wbprojects/", "test.wbproj" };
-	vector<File> srcfiles;
+struct SourceFile {
+	string fpath, fname;
+	bool loaded = false;
+	vector<string> lines;
+
+	string fullpath() { return fpath + fname; }
 
 	int load() {
-		fstream fs(ipfile.fname(), ios::in);
+		fstream fs(fullpath(), ios::in);
 		if (!fs.is_open())
-			return fprintf(stderr, "error opening file: '%s'\n", ipfile.fname().c_str()), 1;
-		
-		printf("opened project file: '%s'\n", ipfile.name.c_str());
-		string line, s;
-		vector<string> lines;
+			return fprintf(stderr, "error opening file: '%s'\n", fullpath().c_str()), 1;
+		printf("opened source file: '%s'\n", fname.c_str());
+		string line;
 		while (getline(fs, line))
 			lines.push_back(line);
-		fs.close();
+		return 0;
+	}
+};
 
-		stringstream ss;
-		for (size_t i = 0; i < lines.size(); i++) {
-			ss.str(lines[i]), ss.clear();
-			ss >> s;
+// interpreter project
+struct IProject : SourceFile {
+	vector<SourceFile> srcfiles;
+
+	int load() {
+		fpath = "./wbprojects/";
+		fname = "test.wbproj";
+		int err = SourceFile::load();
+		if (err)  return err;
+
+		// load & parse project file 
+		for (int i = 0; i < (int)lines.size(); i++) {
+			string s = clamp(lines[0]);
 			if (s.length() == 0 || s[0] == '#') ;
+			else if (lines[i][0] == '\t')  printf("unexpected indentation on line %d\n", i);
+			else if (s == "name:") ;  // todo
+			else if (s == "version:") ;  // todo
 			else if (s == "source:") {
-				for (i++; i < lines.size(); i++) {
+				for (i++; i < (int)lines.size(); i++) {
 					if (lines[i].length() == 0 || lines[i][0] != '\t')  break;
-					ss.str(lines[i]), ss.clear();
-					ss >> s;
-					srcfiles.push_back({ ipfile.path, s });
+					srcfiles.push_back({ fpath, clamp(lines[i]) });
 				}
 			}
 			else
-				return fprintf(stderr, "unknown command on line %d: '%s'\n", (int)i, s.c_str()), 2;
+				return fprintf(stderr, "unknown command on line %d: '%s'\n", i, s.c_str()), 2;
+		}
+
+		// load wbas source files
+		for (auto& s : srcfiles) {
+			int err = s.load();
+			if (err)  return err;
 		}
 
 		return 0;
 	}
 
-	// string strip(const string& s) {
-	// 	string r;
-	// 	for (int i = 0; i < s.length(); i++)
-	// }
+	string clamp(const string& s) {
+		int i = 0, j = int(s.length())-1;
+		for (; i < (int)s.length(); i++)
+			if (!isspace(s[i]))  break;
+		for (; j >= 0; j--)
+			if (!isspace(s[j]))  break;
+		return s.substr(i, j-i+1);
+	}
 
 	void report() {
-		printf("iproject: %s (%s)\n", ipfile.name.c_str(), ipfile.fname().c_str());
+		printf("iproject: %s (%s)\n", fname.c_str(), fullpath().c_str());
 		printf("source:\n");
 		for (const auto& f : srcfiles)
-			printf("  %s\n", f.name.c_str());
+			printf("  %s\n", f.fname.c_str());
 	}
 };
